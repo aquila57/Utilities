3f94897d10be90c0ac0d978b9f44d45163b48bad
aquila57 <58740691+aquila57@users.noreply.github.com>
2020-08-13 15:24:39 -0500

    syncing inst.sh and deinst.sh

--- Digital_Clock/deinst.sh
+++ Digital_Clock/deinst.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+make -f clklib.mak clean
 make -f timlib.mak clean
+make -f clk.mak clean
+make -f bigclk.mak clean
 make -f tim.mak clean
 make -f timup.mak clean
--- Digital_Clock/inst.sh
+++ Digital_Clock/inst.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+make -f clklib.mak
 make -f timlib.mak
+make -f clk.mak
+make -f bigclk.mak
 make -f tim.mak
 make -f timup.mak
