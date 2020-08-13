9444c0f8a2f51469994d9ea798041433353809a5
aquila57 <58740691+aquila57@users.noreply.github.com>
2020-08-13 12:09:10 -0500

    Removed two test programs

--- Analog_Clock/deinst.sh
+++ Analog_Clock/deinst.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 make -f clk.mak clean
-make -f gfclk.mak clean
-make -f shwtim.mak clean
 make -f stopw.mak clean
 make -f timer.mak clean
--- Analog_Clock/inst.sh
+++ Analog_Clock/inst.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 make -f clk.mak
-make -f gfclk.mak
-make -f shwtim.mak
 make -f stopw.mak
 make -f timer.mak
